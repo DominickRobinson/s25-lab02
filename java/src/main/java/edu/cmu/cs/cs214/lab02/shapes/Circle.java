91a60c3cfa586478b1654c2fa5efd71f70f1982b
Dominick Robinson <drobinson.lbe@gmail.com>
2025-01-24 09:41:36 -0500

    fixed bug

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
@@ -1,32 +1,13 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Circle implements Shape {
-    private double radius;
+  private double radius;
 
-    public Circle(double radius) {
-        this.radius = radius;
-    }
-
-    public double getArea() {
-        return Math.PI * radius * radius;
-    }
-}
-
-class Rectangle implements Shape {
-  private width: number;
-  private height: number;
-
-  constructor(width: number, height: number) {
-    this.width = width;
-    this.height = height;
+  public Circle(double radius) {
+    this.radius = radius;
   }
 
-  public getArea(): number {
-    return this.width * this.height;
+  public double getArea() {
+    return Math.PI * radius * radius;
   }
 }
-
-    export
-
-    { Rectangle }
-;
